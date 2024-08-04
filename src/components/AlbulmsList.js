3a91d55a470ae0764a152b20379328a352e383f7
Noskinpain <ebukasam108@gmail.com>
2024-08-04 13:10:16 +0100

    added albumlist

--- src/components/AlbulmsList.js
+++ src/components/AlbulmsList.js
@@ -1,8 +1,45 @@
 import React from 'react'
+import { useFetchAlbumsQuery } from '../store'
+import Skeleton from "./Skeleton"
+import Button from "./Button"
+import ExpandablePanel from "./ExpandablePanel"
+import { useAddAlbumMutation } from '../store'
 
 const AlbulmsList = ({user}) => {
+   const {data, error, isLoading} = useFetchAlbumsQuery(user)
+   const [addAlbum, results] = useAddAlbumMutation()
+
+//    console.log(data, error, isLoading)
+
+const handleAddAlbum = () => {
+  addAlbum(user)
+}
+let content;
+if(isLoading){
+    content = <Skeleton times={3}/>
+}
+else if(error){
+    content = <div>Error loading albums.</div>
+} else{
+    content = data.map((album) => {
+        const header = <div>{album.title}</div>
+        return <ExpandablePanel key={album.id} header={header}>
+          list of photos in the album
+        </ExpandablePanel>
+    })
+}
+
   return (
-    <div>AlbulmsList for {user.name}</div>
+    <div>
+    <div>Albulms for {user.name}
+        <Button onClick = {handleAddAlbum}>
+            +Add Album
+        </Button>
+    </div>
+     <dov>
+        {content}
+     </dov>
+    </div>
   )
 }
 
